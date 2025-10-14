7e0c21642e402214ad0a05bb136a4cccfc7ea6d9
Aleeeeeh <alefesilva097@gmail.com>
2025-10-14 06:12:46 -0300

    Atualiza configurações de banco de dados e migrações
    Adicionadas instruções `using` para os namespaces `ApiFinancas.Data`, `ApiFinancas.Infrastructure.Configurations` e `Microsoft.EntityFrameworkCore`. Implementada a criação de um escopo de serviço para executar a migração do banco de dados ao iniciar a aplicação, garantindo que o esquema esteja atualizado.

--- ApiFinancas/Program.cs
+++ ApiFinancas/Program.cs
@@ -1,5 +1,7 @@
 using AFSilva.Core.Auth;
+using ApiFinancas.Data;
 using ApiFinancas.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,12 @@ builder.Services.AddDatabaseConfiguration(builder.Configuration);
 builder.Services.AddDependencyInjection(builder.Configuration);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
